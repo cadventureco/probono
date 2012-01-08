a1446932891c9a56614007fabb84fbb90c2beb5e
David Koo <dkoo@rocketmail.com>
2012-01-08 15:03:49 -0500

    created ruby project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Probono::Application.config.secret_token = '8147984835013c1213d853603997efe319ba94d7ec4fb0d7492f959ed6a3cfa8e968cebe2e19b4517dbc915914cbbacff56b42f1acab6d2e251265470eaf020d'
